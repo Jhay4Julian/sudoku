600a8aa5342c46f6848216ae9a810de2cadae083
Julian Koripamo <jkoripamo@gmail.com>
2025-01-18 14:19:32 +0100

    isValid function: checking row

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ type Board = number[][];
 
 // Check if placing a number is valid
 function isValid(board: Board, row: number, col: number, num: number): boolean {
-    
+    // Check row
+    if (board[row].includes(num)) {
+        return false;
+    }
+
 
     return true;
 }
