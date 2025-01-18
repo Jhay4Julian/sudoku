aba2f5cb631ecf9d7d186c2261c0b075f9799b89
Julian Koripamo <jkoripamo@gmail.com>
2025-01-18 14:18:49 +0100

    A function to check if placing a number is valid

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 // SODUKU LOGIC
+
+type Board = number[][];
+
+// Check if placing a number is valid
+function isValid(board: Board, row: number, col: number, num: number): boolean {
+    
+
+    return true;
+}
